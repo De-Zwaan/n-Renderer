5fafb244fd7c7ce8adf9ba3fa76c5735a0802bf3
De-Zwaan <46651802+De-Zwaan@users.noreply.github.com>
2023-10-28 16:16:24 +0200

    Bump winit to 0.29

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::f32::consts::PI;
 
 // Crates for window managment
-use pixels::{Error, PixelsBuilder, SurfaceTexture};
+use pixels::{PixelsBuilder, SurfaceTexture};
 use winit::{
     dpi::LogicalSize,
     event::{Event, WindowEvent},
-    event_loop::EventLoop,
-    window::WindowBuilder,
+    event_loop::{EventLoop, ControlFlow},
+    window::WindowBuilder, error::EventLoopError,
 };
 
 // Actual rendering code
@@ -17,8 +17,8 @@ const HEIGHT: usize = 600;
 
 const SCALE: f32 = 200.0;
 
-fn main() -> Result<(), Error> {
-    let event_loop = EventLoop::new();
+fn main() -> Result<(), EventLoopError> {
+    let event_loop = EventLoop::new().unwrap();
 
     // Initialise the window
     let window = WindowBuilder::new()
@@ -38,7 +38,7 @@ fn main() -> Result<(), Error> {
     );
 
     // Create a pixelarray
-    let mut pixels: pixels::Pixels = PixelsBuilder::new(WIDTH as u32, HEIGHT as u32, surface_texture).build()?;
+    let mut pixels: pixels::Pixels = PixelsBuilder::new(WIDTH as u32, HEIGHT as u32, surface_texture).build().unwrap();
 
     // let mut t: u64 = 0;
 
@@ -51,17 +51,17 @@ fn main() -> Result<(), Error> {
 
     // shape.rotate(RotationPlane::get_rot_mat_4d(RotationPlane::YZ, PI / 2.0));
 
-    event_loop.run(move |event, _, control_flow| {
-        control_flow.set_poll();
+    event_loop.set_control_flow(ControlFlow::Poll);
 
+    event_loop.run(move |event: Event<()>, control_flow| {
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
             } => {
                 // println!("Window closed");
-                control_flow.set_exit();
-            }
+                control_flow.exit();
+            },
             Event::WindowEvent {
                 event: WindowEvent::Resized(new_size),
                 ..
@@ -69,13 +69,11 @@ fn main() -> Result<(), Error> {
                 // println!("Window resized");
                 let _ = pixels.resize_buffer(new_size.width, new_size.height);
                 let _ = pixels.resize_surface(new_size.width, new_size.height);
-            }
-            Event::MainEventsCleared => {
-                window.request_redraw();
-            }
-            Event::RedrawRequested(_) => {
-                // t += 1;
-
+            },
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
                 let screen = pixels.frame_mut();
                 let mut depth_buffer = [None; WIDTH * HEIGHT];
 
@@ -96,9 +94,9 @@ fn main() -> Result<(), Error> {
                     .map_err(|e| println!("pixels.render() failed: {}", e))
                     .is_err()
                 {
-                    control_flow.set_exit();
+                    control_flow.exit();
                 };
-            }
+            },
             _ => (),
         }
     })
